bc41debeab0295d4ba1df72f3d3799c04d3b9e2e
Said Kadrioski <said@kadrioski.de>
2026-04-28 13:39:58 +0200

    Benchmark over many parameters

--- bench/Bench.scala
+++ bench/Bench.scala
@@ -8,12 +8,15 @@ import EvalResult.*
 
 import org.openjdk.jmh.annotations.*
 import scala.language.implicitConversions
-import org.openjdk.jmh.infra.Blackhole
 import scala.compiletime.uninitialized
+
+import java.util.concurrent.atomic.AtomicInteger
 
 def parser: Parser[Char, Expr] = WrappedParser(strictExprEnter).asInstanceOf
 
-@BenchmarkMode(Mode.AverageTime)
+val rand = scala.util.Random()
+
+@BenchmarkMode(Array(Mode.AverageTime))
 @State(Scope.Benchmark)
 class Bench {
   @Param(Array("100", "200", "400"))
@@ -22,24 +25,57 @@ class Bench {
   @Param(Array("0.05", "0.1", "0.2", "0.4"))
   var fp_rate: Double = uninitialized
 
+  @Param(Array("2", "3", "4"))
+  var bf: Int = uninitialized
+
+  private val dfs_success_count = AtomicInteger(0)
+  private val dfs_total_tests = AtomicInteger(0)
+  private val ra_success_count = AtomicInteger(0)
+  private val ra_total_tests = AtomicInteger(0)
+  private val gu_success_count = AtomicInteger(0)
+  private val gu_total_tests = AtomicInteger(0)
+
   def seed(): Node[Char] =
-    seedWCFG(token_length, typoRateFromFpRate(fp_rate, token_length))
+    seedWCFG(token_length, typoRateFromFpRate(fp_rate, token_length), bf, rand)
 
   @Benchmark
   def dfsEval() = {
-    val tg = new DFS_TG(seed)
-    StackEvaluator.eval(parser, tg)
+    val tg = new DFS_TG(seed, token_length * 5)
+    dfs_total_tests.incrementAndGet()
+    StackEvaluator.eval(parser, tg) match {
+      case Success(_) => dfs_success_count.incrementAndGet()
+      case _          => dfs_success_count.get()
+    }
   }
 
   @Benchmark
   def retryAllEval() = {
     val tg = new RetryAll_TG(seed)
-    ScrapAllEvaluator.eval(parser, tg)
+    ra_total_tests.incrementAndGet()
+    ScrapAllEvaluator.eval(parser, tg) match {
+      case Success(_) => ra_success_count.incrementAndGet()
+      case _          => ra_success_count.get()
+    }
   }
 
   @Benchmark
   def giveUpEval() = {
     val tg = new GiveUp_TG(seed)
-    ScrapAllEvaluator.eval(parser, tg)
+    gu_total_tests.incrementAndGet()
+    ScrapAllEvaluator.eval(parser, tg) match {
+      case Success(_) => gu_success_count.incrementAndGet()
+      case _          => gu_success_count.get()
+    }
+  }
+
+  @TearDown(Level.Trial)
+  def successRates() = {
+    val dfs_rate =
+      dfs_success_count.doubleValue() / dfs_total_tests.doubleValue()
+    val ra_rate = ra_success_count.doubleValue() / ra_total_tests.doubleValue()
+    val gu_rate = gu_success_count.doubleValue() / gu_total_tests.doubleValue()
+    println(s"Success rate of DFS: $dfs_rate")
+    println(s"Success rate of RetryAll: $ra_rate")
+    println(s"Success rate of GiveUp: $gu_rate")
   }
 }
