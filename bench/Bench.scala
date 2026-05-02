48aa700d684a050efedffe7d1584a4a59da8b1b9
Said Kadrioski <said@kadrioski.de>
2026-05-02 17:48:57 +0200

    Finalize benchmark

--- bench/Bench.scala
+++ bench/Bench.scala
@@ -16,7 +16,7 @@ def parser: Parser[Char, Expr] = WrappedParser(strictExprEnter).asInstanceOf
 
 val rand = scala.util.Random()
 
-@BenchmarkMode(Array(Mode.AverageTime))
+@BenchmarkMode(Array(Mode.AverageTime, Mode.Throughput))
 @State(Scope.Benchmark)
 class Bench {
   @Param(Array("100", "200", "400"))
@@ -25,57 +25,78 @@ class Bench {
   @Param(Array("0.05", "0.1", "0.2", "0.4"))
   var fp_rate: Double = uninitialized
 
-  @Param(Array("2", "3", "4"))
-  var bf: Int = uninitialized
+  private val success_count = AtomicInteger(0)
+  private val failure_count = AtomicInteger(0)
 
-  private val dfs_success_count = AtomicInteger(0)
-  private val dfs_total_tests = AtomicInteger(0)
-  private val ra_success_count = AtomicInteger(0)
-  private val ra_total_tests = AtomicInteger(0)
-  private val gu_success_count = AtomicInteger(0)
-  private val gu_total_tests = AtomicInteger(0)
+  def seed1(): Node[Char] =
+    seedWCFG(token_length, typoRateFromFpRate(fp_rate, token_length), 1, rand)
 
-  def seed(): Node[Char] =
-    seedWCFG(token_length, typoRateFromFpRate(fp_rate, token_length), bf, rand)
+  def seed2(): Node[Char] =
+    seedWCFG(token_length, typoRateFromFpRate(fp_rate, token_length), 2, rand)
+
+  def seed3(): Node[Char] =
+    seedWCFG(token_length, typoRateFromFpRate(fp_rate, token_length), 3, rand)
+
+  def seed4(): Node[Char] =
+    seedWCFG(token_length, typoRateFromFpRate(fp_rate, token_length), 4, rand)
 
   @Benchmark
-  def dfsEval() = {
-    val tg = new DFS_TG(seed, token_length * 5)
-    dfs_total_tests.incrementAndGet()
+  def dfsEvalBF2() = {
+    val tg = new DFS_TG(seed2, token_length * 5)
     StackEvaluator.eval(parser, tg) match {
-      case Success(_) => dfs_success_count.incrementAndGet()
-      case _          => dfs_success_count.get()
+      case Success(_) => success_count.incrementAndGet()
+      case _          => failure_count.incrementAndGet()
+    }
+  }
+
+  @Benchmark
+  def dfsEvalBF3() = {
+    val tg = new DFS_TG(seed3, token_length * 5)
+    StackEvaluator.eval(parser, tg) match {
+      case Success(_) => success_count.incrementAndGet()
+      case _          => failure_count.incrementAndGet()
+    }
+  }
+
+  @Benchmark
+  def dfsEvalBF4() = {
+    val tg = new DFS_TG(seed4, token_length * 5)
+    StackEvaluator.eval(parser, tg) match {
+      case Success(_) => success_count.incrementAndGet()
+      case _          => failure_count.incrementAndGet()
     }
   }
 
   @Benchmark
   def retryAllEval() = {
-    val tg = new RetryAll_TG(seed)
-    ra_total_tests.incrementAndGet()
+    val tg = new RetryAll_TG(seed1)
     ScrapAllEvaluator.eval(parser, tg) match {
-      case Success(_) => ra_success_count.incrementAndGet()
-      case _          => ra_success_count.get()
+      case Success(_) => success_count.incrementAndGet()
+      case _          => failure_count.incrementAndGet()
     }
   }
 
   @Benchmark
   def giveUpEval() = {
-    val tg = new GiveUp_TG(seed)
-    gu_total_tests.incrementAndGet()
+    val tg = new GiveUp_TG(seed1)
     ScrapAllEvaluator.eval(parser, tg) match {
-      case Success(_) => gu_success_count.incrementAndGet()
-      case _          => gu_success_count.get()
+      case Success(_) => success_count.incrementAndGet()
+      case _          => failure_count.incrementAndGet()
     }
   }
 
   @TearDown(Level.Trial)
   def successRates() = {
-    val dfs_rate =
-      dfs_success_count.doubleValue() / dfs_total_tests.doubleValue()
-    val ra_rate = ra_success_count.doubleValue() / ra_total_tests.doubleValue()
-    val gu_rate = gu_success_count.doubleValue() / gu_total_tests.doubleValue()
-    println(s"Success rate of DFS: $dfs_rate")
-    println(s"Success rate of RetryAll: $ra_rate")
-    println(s"Success rate of GiveUp: $gu_rate")
+    val sc = success_count.get()
+    val fc = failure_count.get()
+    val tc = sc + fc
+    val rate = sc.toDouble / tc.toDouble
+
+    println(s"------------------------------")
+    println(s"Success rate: $rate")
+    println(s"Success count: $sc")
+    println(s"Failure count: $fc")
+    println(s"Total count: $tc")
+    println(s"------------------------------")
   }
 }
